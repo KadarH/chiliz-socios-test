590b0f1b3bcd13b084a68a94d26d8e56872b3a00
kadarH <kadar.hamza@gmail.com>
2022-12-28 17:43:17 +0000

    add pure js function implementation

---
+++ main.js
@@ -0,0 +1,59 @@
+/*
+ * Your program must print string with the number of years and months and the total number of days between the dates.
+ * Dates are provided in dd.mm.yyyy format.
+ * You are not allowed to plug in JS libraries such as moment.js or date-fns directly into the code. All code need to be written in this file.
+ * 
+ * Result must be shown as a string in years, months and total days. If years or months are 0, then it should not be displayed in the output.
+ *
+ * Example:
+ * Input: ['01.01.2000', '01.01.2016']
+ * Output:
+ * '16 years, total 5844 days'
+ *
+ * Example 2:
+ * Input: ['01.11.2015', '01.02.2017']
+ *
+ * Output:
+ * '1 year, 3 months, total 458 days'
+*/
+const dates = [
+    ['01.01.2000', '01.01.2016'],
+    ['01.01.2016', '01.08.2016'],
+    ['01.11.2015', '01.02.2017'],
+    ['17.12.2016', '16.01.2017'],
+    ['01.01.2016', '01.01.2016'],
+    ['28.02.2015', '13.04.2018'],
+    ['28.01.2015', '28.02.2015'],
+    ['17.03.2022', '17.03.2023'],
+    ['17.02.2024', '17.02.2025'],
+];
+
+// Receive string of dates one after each other
+function outputDate(dates) {
+
+    const difference = {
+        inDays: (d1, d2) => {
+            return Math.floor((d2.getTime() - d1.getTime()) / (24 * 3600 * 1000));
+        },
+        inMonths: (d1, d2) => {
+            return parseInt((((difference.inDays(d1, d2) / 365) - difference.inYears(d1, d2)) * 365) / 30);
+        },
+        inYears: (d1, d2) => {
+            return d2.getFullYear() - d1.getFullYear() - (d2.getMonth() < d1.getMonth() ? 1 : 0);
+        }
+    }
+
+    const date0 = dates[0].split('.');
+    const date1 = dates[1].split('.');
+    var d1 = new Date(date0[1] + '-' + date0[0] + '-' + date0[2]);
+    var d2 = new Date(date1[1] + '-' + date1[0] + '-' + date1[2]);
+
+    const days = difference.inDays(d1, d2);
+    const months = difference.inMonths(d1, d2);
+    const years = difference.inYears(d1, d2);
+
+    return (years ? years + " year" + (years > 1 ? "s" : "") : "")
+        + (months % 12 > 0 && (days !== 30 || days === 0) ? ((years ? ", " : "") + (months % 12)) + (months % 12 == 1 ? " month" : " months") : "")
+        + (days > 1 && days !== 30 ? ", " : "")
+        + "total " + days + " days";
+}
